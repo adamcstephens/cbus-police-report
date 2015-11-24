3bb5df0297c0ade4b5491874a9be83a861acff25
Adam Stephens <astephens@covermymeds.com>
2015-11-24 10:54:03 -0500

    add some comments

--- getreport.py
+++ getreport.py
@@ -14,14 +14,16 @@ daybefore = date.today() - timedelta(2)
 todate = yesterday.strftime("%m/%d/%Y")
 fromdate = daybefore.strftime("%m/%d/%Y")
 
+# get an MX server for an email address
 def get_mx_host(email):
     domain = str.split(email, '@')[1]
     answers = dns.resolver.query(domain, 'MX')
-    # return first MX host
+    # return first MX host, removing the final .
     return str(answers[0].exchange)[:-1]
 
-
+# fetch the report
 def get_police_report(district,incident_types='9'):
+    # if not all, then we need to append dis to the district number
     if district != 'all':
         district = 'dis'+district
     url = 'http://www.columbuspolice.org/reports/Results?from=%s&to=%s&loc=%s&types=%s' % (fromdate, todate, district, incident_types)
@@ -35,17 +37,20 @@ def send_email(fromemail, toemail, htmlcontent):
     else:
         mailserver = args.mailserver
 
+    # prepare the email
     msg = MIMEMultipart('alternative')
     msg['Subject'] = "Local Police Reports: %s - %s" % (fromdate, todate)
     msg['From'] = fromemail
     msg['To'] = toemail
 
+    # set the content
     textcontent = 'No text content'
     part1 = MIMEText(textcontent, 'plain')
     part2 = MIMEText(htmlcontent, 'html')
     msg.attach(part1)
     msg.attach(part2)
 
+    # send the email
     s = smtplib.SMTP(mailserver)
     s.sendmail(fromemail, toemail, msg.as_string())
     s.quit()
